7cb3e9d41e0f517ad0e2240e05c3256888932ea6
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-05-28 07:33:57 -0400

    finished w7d1 hw

--- w7d1/app_name/app/models/user.rb
+++ w7d1/app_name/app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: { message: "Password can't be blank"}
-  validates :password_digest, length: { minimum: 6, allow_nil: true}
+  validates :password, length: { minimum: 6, allow_nil: true}
   before_validation :ensure_session_token
   
   attr_reader :password
 
   def self.find_by_credentials(username, password)
-    user = User.find_by(username: username, password)
+    user = User.find_by(username: username)
     return user if user && BCrypt::Password.new(user.password_digest).is_password(password)
     nil
   end 
